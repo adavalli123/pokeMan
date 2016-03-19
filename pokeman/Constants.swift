b544d5415626798060d14fdee4c822a6182a1273
Srikanth Adavalli <sadavalli@beachbody.com>
2016-03-19 15:24:07 -0700

    Asynchronous Web Service Calls updated

---
+++ pokeman/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  pokeman
+//
+//  Created by Srikanth Adavalli on 3/18/16.
+//  Copyright © 2016 Srikanth Adavalli. All rights reserved.
+//
+
+import Foundation
---
+++ pokeman/PokemonVC.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonVC.swift
+//  pokeman
+//
+//  Created by Srikanth Adavalli on 3/17/16.
+//  Copyright © 2016 Srikanth Adavalli. All rights reserved.
+//
+
+import Cocoa
+
+class PokemonVC: UIViewController {
+
+}
